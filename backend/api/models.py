e34979e5445fd599d8e9d4009cfc65071e26e61f
Harshit saxena <saxena24harshit@gmail.com>
2026-02-10 19:55:09 +0530

    change supabase account

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,7 +15,7 @@ class Match(models.Model):
     time_date = models.DateTimeField()
     location = models.CharField(max_length=200)
 
-class Attendance(models.Model):
+class AttendanceStats(models.Model):
     player = models.OneToOneField(Player, on_delete=models.CASCADE)
     percentage = models.DecimalField(max_digits=5, decimal_places=2)
     date_present = models.JSONField(default=list)
@@ -23,3 +23,21 @@ class Attendance(models.Model):
     fee_status = models.BooleanField(default=True) # could be boolean
     def __str__(self):
         return self.player.name + " attendance"
+
+class Attendance(models.Model):
+    player = models.OneToOneField(Player, on_delete=models.CASCADE)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    date_present = models.JSONField(default=list)
+    date_absent = models.JSONField(default=list)
+    fee_status = models.BooleanField(default=True)
+    def __str__(self):
+        return self.player.name + " attendance"
+
+# class AttendancePerday(models.Model):
+#     player = models.OneToOneField(Player, on_delete=models.CASCADE)
+#     percentage = models.DecimalField(max_digits=5, decimal_places=2)
+#     date_present = models.JSONField(default=list)
+#     date_absent = models.JSONField(default=list)
+#     fee_status = models.BooleanField(default=True)
+#     def __str__(self):
+#         return self.player.name + " attendance"
