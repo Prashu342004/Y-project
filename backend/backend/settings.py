dcab632a668d38a5bd70f5dbd636311d49a610a4
shubh mishra <mshubh745@gmail.com>
2026-02-18 00:35:24 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,18 +25,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 from decouple import config
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'), 
-        'PORT': '5432',
-          'OPTIONS': {
-            'sslmode': 'require',
-        }
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': config('DB_NAME'),
+    'USER': config('DB_USER'),
+    'PASSWORD': config('DB_PASSWORD'),
+    'HOST': config('DB_HOST'),
+    'PORT': '5432',
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
