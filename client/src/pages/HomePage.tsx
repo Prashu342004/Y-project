658b0787890eb8ae6d2b3a48f2f7440602cdc67d
Harshit saxena <saxena24harshit@gmail.com>
2026-02-26 17:10:29 +0530

    fix ping code snippet

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
 import { Hero } from "../components/Hero";
 import { ImageCarousel } from "../components/ImageCarousel";
 import { PlayersSection } from "../components/PlayersSection";
 import { UpcomingMatch } from "../components/UpcomingMatch";
 import { useFetch } from "../lib/fetch";
+import { api } from "../lib/axiosInstance";
 
 export function HomePage() {
-  const { data } = useFetch("",'get');
-  setInterval(()=>{
-    const responseFromPing = useFetch("/ping",'get');
-    console.log(responseFromPing); 
-  },30000)
+  const { data } = useFetch("", 'get');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      api.get("/ping")
+        .then(res => {
+          console.log(res.data?.success);
+        })
+        .catch(err => {
+          console.error("Ping failed:", err);
+        });
+    }, 30000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   let Player_data = data?.players_detail;
   const MatchDetails = data?.upcoming_matches;
 
